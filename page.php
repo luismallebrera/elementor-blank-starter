afe6c6eb79ce5816fb5fa50c58fa4b31b2dd94e8
luismallebrera <38189359+luismallebrera@users.noreply.github.com>
2025-11-25 23:59:04 +0000

    Move transition-pannel-bg before transition-borders-bg in DOM order

--- page.php
+++ page.php
@@ -14,8 +14,8 @@ get_footer();
 
 // Page transition elements
 if (get_theme_mod('enable_page_transitions', false)) : ?>
+    <div aria-hidden="true" class="transition-pannel-bg initial-load"></div>
     <?php if (get_theme_mod('enable_page_transitions_borders', true)) : ?>
         <div aria-hidden="true" class="transition-borders-bg"></div>
     <?php endif; ?>
-    <div aria-hidden="true" class="transition-pannel-bg initial-load"></div>
 <?php endif;
