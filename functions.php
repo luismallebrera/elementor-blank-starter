2582c566f680f811c5c616b4bc3bdf86eefd6b34
luismallebrera <38189359+luismallebrera@users.noreply.github.com>
2025-11-25 22:32:16 +0000

    Remove body.close opacity fade for Elementor content

--- functions.php
+++ functions.php
@@ -87,7 +87,7 @@ function elementor_blank_scripts() {
             'elementor-blank-page-transitions',
             get_template_directory_uri() . '/css/page-transitions.css',
             array(),
-            '3.1'
+            '3.2'
         );
         
         // Add inline CSS for dynamic settings
