ad357337660535d06f08b593164e9b20df7d9460
luismallebrera <38189359+luismallebrera@users.noreply.github.com>
2025-11-26 02:00:12 +0000

    Add page transition elements to single.php and index.php

--- index.php
+++ index.php
@@ -23,6 +23,15 @@ if (have_posts()) :
 endif;
 ?>
 
+<?php
+// Page transition elements
+if (get_theme_mod('enable_page_transitions', false)) : ?>
+    <div aria-hidden="true" class="transition-pannel-bg initial-load"></div>
+    <?php if (get_theme_mod('enable_page_transitions_borders', true)) : ?>
+        <div aria-hidden="true" class="transition-borders-bg"></div>
+    <?php endif; ?>
+<?php endif; ?>
+
 <?php wp_footer(); ?>
 </body>
 </html>
