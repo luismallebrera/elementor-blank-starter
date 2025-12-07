33f9d9652518d6b863dae73c65d022ce71d6bc41
luismallebrera <38189359+luismallebrera@users.noreply.github.com>
2025-12-07 21:07:54 +0000

    Add JS to modify Entry List links for page reload with municipio_id

--- inc/municipio-popup-support.php
+++ inc/municipio-popup-support.php
@@ -9,12 +9,23 @@ if (!defined('ABSPATH')) {
 }
 
 /**
- * JavaScript to auto-open popup and reload page when clicking municipio links
+ * JavaScript to auto-open popup and modify entry list links
  */
 function elementor_blank_popup_auto_open_script() {
     ?>
     <script>
     jQuery(document).ready(function($) {
+        // Modify Entry List links to add municipio_id and reload page
+        $('.entry-list-item a, .elementor-post a').each(function() {
+            var $link = $(this);
+            var postId = $link.closest('[data-id]').attr('data-id') || $link.attr('data-id');
+            
+            if (postId) {
+                var currentUrl = window.location.pathname;
+                $link.attr('href', currentUrl + '?municipio_id=' + postId);
+            }
+        });
+        
         // Auto-open popup if municipio_id is in URL
         const urlParams = new URLSearchParams(window.location.search);
         const municipioId = urlParams.get('municipio_id');
