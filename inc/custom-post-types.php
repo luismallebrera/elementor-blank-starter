ac7a733aee2b7efa8ace2b87b27209c4d9a43319
luismallebrera <38189359+luismallebrera@users.noreply.github.com>
2025-12-07 20:52:18 +0000

    Add debug messages to municipio_info shortcode

--- inc/custom-post-types.php
+++ inc/custom-post-types.php
@@ -612,8 +612,9 @@ function elementor_blank_show_municipio_info($atts) {
     // Get municipio_id from URL
     $municipio_id = isset($_GET['municipio_id']) ? absint($_GET['municipio_id']) : 0;
     
+    // Debug output
     if (!$municipio_id) {
-        return '';
+        return '[No municipio_id in URL]';
     }
     
     $field = $atts['field'];
@@ -624,18 +625,24 @@ function elementor_blank_show_municipio_info($atts) {
             
         case 'galgdr':
             $galgdr_id = get_post_meta($municipio_id, '_municipio_galgdr_asociado', true);
-            return $galgdr_id ? get_the_title($galgdr_id) : '';
+            if (!$galgdr_id) {
+                return '[No GAL/GDR for municipio ' . $municipio_id . ']';
+            }
+            return get_the_title($galgdr_id);
             
         case 'provincia':
             $provincia_id = get_post_meta($municipio_id, '_municipio_provincia', true);
-            if ($provincia_id) {
-                $term = get_term($provincia_id, 'provincia');
-                return ($term && !is_wp_error($term)) ? $term->name : '';
+            if (!$provincia_id) {
+                return '[No provincia for municipio ' . $municipio_id . ']';
             }
-            return '';
+            $term = get_term($provincia_id, 'provincia');
+            if (!$term || is_wp_error($term)) {
+                return '[Invalid provincia ID ' . $provincia_id . ']';
+            }
+            return $term->name;
             
         default:
-            return '';
+            return '[Invalid field: ' . $field . ']';
     }
 }
 add_shortcode('municipio_info', 'elementor_blank_show_municipio_info');
